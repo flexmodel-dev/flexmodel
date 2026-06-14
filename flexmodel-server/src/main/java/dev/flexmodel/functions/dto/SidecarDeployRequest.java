498078bc7cc814170e45e059ec9a9c2d33b8a957
Cheng Jinbao <cjbi@outlook.com>
2026-06-14 10:47:09 +0800

    feat(functions): 添加函数管理功能并修复分支项目ID解析问题

---
+++ flexmodel-server/src/main/java/dev/flexmodel/functions/dto/SidecarDeployRequest.java
@@ -0,0 +1,30 @@
+package dev.flexmodel.functions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * Internal deploy request sent from Java → Deno sidecar.
+ *
+ * @author cjbi
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SidecarDeployRequest {
+
+    private String projectId;
+
+    private String functionId;
+
+    private String name;
+
+    private Map<String, String> sourceFiles;
+
+    private int timeout;
+}

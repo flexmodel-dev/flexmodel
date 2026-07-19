4c1a70f09460c88f557eed563792bd8cf6b016f3
Cheng Jinbao <cjbi@outlook.com>
2026-07-20 00:01:03 +0800

    fix(log): 修复日志消费服务异常处理
    - 移除不必要的 SuppressWarnings 注解
    - 添加 try-catch 块来处理 apiLogService.create 方法的异常
    - 实现 fire and forget 模式的异常处理机制
    - 确保日志消费服务在异常情况下不会中断运行

--- flexmodel-server/src/main/java/dev/flexmodel/api/consumer/LogEventConsumer.java
+++ flexmodel-server/src/main/java/dev/flexmodel/api/consumer/LogEventConsumer.java
@@ -1,10 +1,10 @@
 package dev.flexmodel.api.consumer;
 
+import dev.flexmodel.api.ApiRequestLogService;
+import dev.flexmodel.codegen.entity.ApiRequestLog;
 import io.quarkus.vertx.ConsumeEvent;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import dev.flexmodel.codegen.entity.ApiRequestLog;
-import dev.flexmodel.api.ApiRequestLogService;
 
 import java.util.Map;
 
@@ -17,12 +17,15 @@ public class LogEventConsumer {
   @Inject
   ApiRequestLogService apiLogService;
 
-  @SuppressWarnings("unchecked")
   @ConsumeEvent("request.logging") // 监听特定地址的事件
   public void consume(Map<String, Object> payload) {
     String projectId = (String) payload.get("projectId");
     ApiRequestLog apiLog = (ApiRequestLog) payload.get("log");
-    apiLogService.create(projectId, apiLog);
+    try {
+      apiLogService.create(projectId, apiLog);
+    } catch (Exception _) {
+      // fire and forget
+    }
   }
 
 }
